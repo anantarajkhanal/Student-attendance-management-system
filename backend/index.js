1d4a688fbed988f8b14cc13cb811c83653dcb730
anantaRajkhanal <ananta5441@gmail.com>
2026-03-25 18:13:21 +0545

    Update CORS origin for production deployment

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ const app = express();
 
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://student-attendance-management-syste-eight.vercel.app",
     methods: "GET,POST,PUT,DELETE",
     credentials: true
 }));
